c5927b74016e79402ddc01f61c952fc1ced2f5e8
Sapphire-Coder <74931786+Sapphire-Coder@users.noreply.github.com>
2022-02-15 14:21:03 -0500

    Updated HTML, CSS, and JS files
    Added more sections for HTML file for score keeping, and styled them in CSS. Updated method in JS file to detect if pieces were adjacent

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,7 @@
 class Othello{
     constructor(){
         this.rows = []
-        this.legalMove = null
+        this.legalMove = false
     }
     createRows() { // creates the 8 rows, and fills them with 0 so they have the proper spaces
         for (let i = 0; i < 8; i++){
@@ -13,32 +13,47 @@ class Othello{
         this.rows[4][4] = 1
     }
     legalMoveCheck(y, x) {
-        if(this.rows[y].includes(1)){ // check to see if piece is in the same row
-            this.legalMove = true
-        }
-        for (let i = 0; i < this.rows.length; i++){ // check to see if piece is in the same column
-            if (this.rows[i][x] === 1){
-                this.legalMove = true
+        this.legalMove = false
+        if (this.rows[y][x - 1] > 0 || this.rows[y][x + 1] > 0 || this.rows[y + 1][x] > 0 || this.rows[y - 1][x] > 0 || this.rows[y + 1][x + 1] > 0 || this.rows[y - 1][x + 1] > 0 || this.rows[y + 1][x - 1] > 0 || this.rows[y - 1][x - 1] > 0) {
+            for (let i = x - 1; i >= 0; i--){ // check to see if piece is in same row in reverse
+                if (this.rows[y][i] === 1) {
+                    this.legalMove = true
+                }
             }
-        }
-        for (let i = y + 1 , j = x + 1 ; i < 8 && j < 8; i++, j++){
-            if (this.rows[i][j] === 1){
-                this.legalMove = true
+            for (let i = x + 1; i < 8; i++){ // check to see if piece is in same row forward
+                if (this.rows[y][i] === 1) {
+                    this.legalMove = true
+                }
             }
-        }
-        for (let i = y - 1 , j = x - 1 ; i >= 0 && j >= 0; i--, j--){
-            if (this.rows[i][j] === 1){
-                this.legalMove = true
+            for (let i = y - 1; i >= 0; i--){ // check to see if piece is in the same column in reverse
+                if (this.rows[i][x] === 1){
+                    this.legalMove = true
+                }
             }
-        }
-        for (let i = y - 1 , j = x + 1 ; i >= 0 && j < 8; i--, j++){
-            if (this.rows[i][j] === 1){
-                this.legalMove = true
+            for (let i = y + 1; i < 8; i++){ // check to see if piece is in the same column forward
+                if (this.rows[i][x] === 1){
+                    this.legalMove = true
+                }
             }
-        }
-        for (let i = y + 1 , j = x - 1 ; i < 8 && j >= 0; i++, j--){
-            if (this.rows[i][j] === 1){
-                this.legalMove = true
+            for (let i = y + 1 , j = x + 1 ; i < 8 && j < 8; i++, j++){ // check to see if piece is in down-right diagonal
+                if (this.rows[i][j] === 1){
+                    this.legalMove = true
+                }
+            }
+            for (let i = y - 1 , j = x - 1 ; i >= 0 && j >= 0; i--, j--){ // check to see if piece is in up-left diagonal
+                if (this.rows[i][j] === 1){
+                    this.legalMove = true
+                }
+            }
+            for (let i = y - 1 , j = x + 1 ; i >= 0 && j < 8; i--, j++){ // check to see if piece is in up-right diagonal
+                if (this.rows[i][j] === 1){
+                    this.legalMove = true
+                }
+            }
+            for (let i = y + 1 , j = x - 1 ; i < 8 && j >= 0; i++, j--){ // check to see if piece is in down-left diagonal
+                if (this.rows[i][j] === 1){
+                    this.legalMove = true
+                }
             }
         }
         return this.legalMove
@@ -47,8 +62,7 @@ class Othello{
 
 let x = new Othello
 x.createRows()
-console.log(x.rows)
-console.log(x.legalMoveCheck(7, 7))
+console.log(x.legalMoveCheck(3, 7))
 // document.querySelector('#r5c6').addEventListener('click', function() {
 //     document.appendChild('span'.className('black'))
 // })
