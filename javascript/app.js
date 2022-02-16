e2d95ec112ac749d0c76ab3c28bb19f3e3f5c50d
Sapphire-Coder <74931786+Sapphire-Coder@users.noreply.github.com>
2022-02-16 12:58:53 -0500

    JS file methods update
    included more methods for the class, and updated them to be more modular. Also included random cpu method

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,9 @@ class Othello{
     constructor(){
         this.rows = []
         this.legalMove = false
+        this.blackFlips = []
+        this.whiteFlips = []
+        this.cpuPossibleMoves = []
     }
     createRows() { // creates the 8 rows, and fills them with 0 so they have the proper spaces
         for (let i = 0; i < 8; i++){
@@ -12,17 +15,17 @@ class Othello{
         this.rows[4][3] = 2
         this.rows[4][4] = 1
     }
-    legalMoveCheck(y, x) {
+    legalMoveCheck(playerNum, y, x) {
         this.legalMove = false
-        if (this.rows[y][x - 1] > 0 || this.rows[y][x + 1] > 0 || this.rows[y + 1][x] > 0 || this.rows[y - 1][x] > 0 || this.rows[y + 1][x + 1] > 0 || this.rows[y - 1][x + 1] > 0 || this.rows[y + 1][x - 1] > 0 || this.rows[y - 1][x - 1] > 0) {
+        if ((this.rows[y][(x == 0 ? 1 : x) - 1] > 0 || this.rows[y][(x == 7 ? 6 : x) + 1] > 0 || this.rows[(y == 7 ? 6 : y) + 1][x] > 0 || this.rows[(y == 0 ? 1 : y) - 1][x] > 0 || this.rows[(y == 7 ? 6 : y) + 1][(x == 7 ? 6 : x) + 1] > 0 || this.rows[(y == 0 ? 1 : y) - 1][(x == 7 ? 6 : x) + 1] > 0 || this.rows[(y == 7 ? 6 : y) + 1][(x == 0 ? 1 : x) - 1] > 0 || this.rows[(y == 0 ? 1 : y) - 1][(x == 0 ? 1 : x) - 1] > 0) && this.rows[y][x] == 0) {
             for (let i = x - 1; i >= 0; i--){ // check to see if a black piece is in same row in reverse
-                if (this.rows[y][i] === 1) { // look for the first instance of a black piece
+                if (this.rows[y][i] === playerNum) { // look for the first instance of a black piece
                     // checking first instance of black piece to reference piece is more than 1
-                    if ((Math.abs(this.rows[y].indexOf(1, i) - x)) > 1){
+                    if ((Math.abs(this.rows[y].indexOf(playerNum, i) - x)) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let j = i + 1; j < x; j++){
-                            if (this.rows[y][j] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[y][j] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -33,22 +36,28 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let j = i + 1; j <= x; j++){
-                                this.rows[y][j] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([y, j])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([y, j])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
                     break
                 }
             }
             for (let i = x + 1; i < 8; i++){ // check to see if piece is in same row forward
-                if (this.rows[y][i] === 1) {
+                if (this.rows[y][i] === playerNum) {
                     // checking first instance of black piece to reference piece is more than 1
-                    if ((Math.abs(this.rows[y].indexOf(1, i) - x)) > 1){
+                    if ((Math.abs(this.rows[y].indexOf(playerNum, i) - x)) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let j = i - 1; j > x; j--){
-                            if (this.rows[y][j] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[y][j] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -59,22 +68,28 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let j = i - 1; j >= x; j--){
-                                this.rows[y][j] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([y, j])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([y, j])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
                     break
                 }
             }
             for (let i = y - 1; i >= 0; i--){ // check to see if piece is in the same column in reverse
-                if (this.rows[i][x] === 1){
+                if (this.rows[i][x] === playerNum){
                     // checking first instance of black piece to reference piece is more than 1
                     if (Math.abs(i - y) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let j = i + 1; j < y; j++){
-                            if (this.rows[j][x] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[j][x] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -85,22 +100,28 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let j = i + 1; j <= y; j++){
-                                this.rows[j][x] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([j, x])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([j, x])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
                     break
                 }
             }
             for (let i = y + 1; i < 8; i++){ // check to see if piece is in the same column forward
-                if (this.rows[i][x] === 1){
+                if (this.rows[i][x] === playerNum){
                     // checking first instance of black piece to reference piece is more than 1
                     if (Math.abs(i - y) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let j = i - 1; j > y; j--){
-                            if (this.rows[j][x] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[j][x] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -111,22 +132,28 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let j = i - 1; j >= y; j--){
-                                this.rows[j][x] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([j, x])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([j, x])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
                     break
                 }
             }
             for (let i = y + 1 , j = x + 1 ; i < 8 && j < 8; i++, j++){ // check to see if piece is in down-right diagonal
-                if (this.rows[i][j] === 1){
+                if (this.rows[i][j] === playerNum){
                     // checking first instance of black piece to reference piece is more than 1
                     if (Math.abs(i - y) > 1 && Math.abs(j - x) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let k = i - 1, l = j - 1; k > y && l > x; k--, l--){
-                            if (this.rows[k][l] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[k][l] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -137,22 +164,28 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let k = i - 1, l = j - 1; k >= y && l >= x; k--, l--){
-                                this.rows[k][l] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([k, l])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([k, l])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
                     break
                 }
             }
             for (let i = y - 1 , j = x - 1 ; i >= 0 && j >= 0; i--, j--){ // check to see if piece is in up-left diagonal
-                if (this.rows[i][j] === 1){
+                if (this.rows[i][j] === playerNum){
                     // checking first instance of black piece to reference piece is more than 1
                     if (Math.abs(i - y) > 1 && Math.abs(j - x) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let k = i + 1, l = j + 1; k < y && l < x; k++, l++){
-                            if (this.rows[k][l] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[k][l] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -163,22 +196,28 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let k = i + 1, l = j + 1; k <= y && l <= x; k++, l++){
-                                this.rows[k][l] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([k, l])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([k, l])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
                     break
                 }
             }
             for (let i = y - 1 , j = x + 1 ; i >= 0 && j < 8; i--, j++){ // check to see if piece is in up-right diagonal
-                if (this.rows[i][j] === 1){
+                if (this.rows[i][j] === playerNum){
                     // checking first instance of black piece to reference piece is more than 1
                     if (Math.abs(i - y) > 1 && Math.abs(j - x) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let k = i + 1, l = j - 1; k < y && l > x; k++, l--){
-                            if (this.rows[k][l] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[k][l] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -189,22 +228,28 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let k = i + 1, l = j - 1; k <= y && l >= x; k++, l--){
-                                this.rows[k][l] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([k, l])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([k, l])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
                     break
                 }
             }
             for (let i = y + 1 , j = x - 1 ; i < 8 && j >= 0; i++, j--){ // check to see if piece is in down-left diagonal
-                if (this.rows[i][j] === 1){
+                if (this.rows[i][j] === playerNum){
                     // checking first instance of black piece to reference piece is more than 1
                     if (Math.abs(i - y) > 1 && Math.abs(j - x) > 1){
                         let allWhitePieces = true
                         // checking if any of the pieces inbetween are black or empty
                         for (let k = i - 1, l = j + 1; k > y && l < x; k--, l++){
-                            if (this.rows[k][l] === 1){ // If there is a black piece inbetween, return false
+                            if (this.rows[k][l] === playerNum){ // If there is a black piece inbetween, return false
                                 allWhitePieces = false
                                 break
                             }
@@ -215,26 +260,70 @@ class Othello{
                         }
                         if (allWhitePieces){ // if they all happen to be white, make all of the spaces black
                             for (let k = i - 1, l = j + 1; k >= y && l <= x; k--, l++){
-                                this.rows[k][l] = 1
+                                if (playerNum === 1) {
+                                    this.blackFlips.push([k, l])
+                                    this.legalMove = true
+                                }
+                                else if (playerNum === 2){
+                                    this.whiteFlips.push([k, l])
+                                    this.legalMove = true
+                                }
                             }
                         }
                     }
-                    this.legalMove = true
-                    break
+                    break // breaking after the first instance of same piece
                 }
             }
         }
         return this.legalMove
+    }
+    flipBlackPieces() { // function to add changes to corresponing spaces after legal moves
+        for (let i = 0; i < this.blackFlips.length; i++){ // iterate through the new array and place changes
+            this.rows[this.blackFlips[i][0]][this.blackFlips[i][1]] = 1
+        }
+        this.blackFlips = [] // after making changes, return to an empty array for next legal check move
+    }
+    flipWhitePieces() { // function to add changes to corresponing spaces after legal moves
+        for (let i = 0; i < this.whiteFlips.length; i++){ // iterate through the new array and place changes
+            this.rows[this.whiteFlips[i][0]][this.whiteFlips[i][1]] = 2
+        }
+        this.whiteFlips = [] // after making changes, return to an empty array for next legal check move
+    }
+    cpuRandomMove() { // function for cpu to make a random legal move
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) { // iterating through all and adding legals moves to an array
+                if (this.legalMoveCheck(2, i, j) == true) {
+                    this.cpuPossibleMoves.push([i, j])
+                }
+            }
+        }
+        this.whiteFlips = [] // resetting the array for whiteFlips since running through tests fill it
+        if(this.cpuPossibleMoves.length > 0) {
+            let doMove = Math.floor(Math.random() * this.cpuPossibleMoves.length)
+            this.legalMoveCheck(2, this.cpuPossibleMoves[doMove][0], this.cpuPossibleMoves[doMove][1])
+            this.flipWhitePieces()
+        }
+        this.cpuPossibleMoves = [] // resetting the possible moves array back to empty after running possibilites
     }
 }
 
 let x = new Othello
 x.createRows()
 console.log(x.rows)
-x.rows[5][2] = 1
+// x.rows[5][2] = 1
+// x.rows[3][5] = 2
+// x.rows[4][5] = 2
+// x.rows[5][5] = 1
+// console.log(x.rows)
+// console.log(x.legalMoveCheck(2, 2, 3))
+x.cpuRandomMove()
+// console.log(x.whiteFlips)
+// x.flipWhitePieces()
+// console.log(x.whiteFlips)
+// console.log(x.cpuPossibleMoves)
 console.log(x.rows)
-console.log(x.legalMoveCheck(2, 5))
-console.log(x.rows)
-// document.querySelector('#r5c6').addEventListener('click', function() {
-//     document.appendChild('span'.className('black'))
+// console.log(x.legalMoveCheck(1, 7, 7))
+// document.querySelector('#r3c5').addEventListener('click', function() {
+//     let piece = document.querySelector('#gameBoard').appendChild('#r3c5').createElement('span')
+//     piece.classList('black')
 // })
